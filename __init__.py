1d391e6166a3a1b70b8e0178e306bb723b2b334c
Samuel Bear Powell <samuel.powell@uq.edu.au>
2018-12-13 17:25:08 +1000

    add script_utils, mpl_utils, and stats_utils to __all__

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 #let's screw with the import path... why not?
 __path__.append(__import__('os').path.join(__path__[-1],'python'))
 
-__all__ = ['ansi','progress']
+__all__ = ['ansi','progress','script_utils','mpl_utils','stats_utils']
