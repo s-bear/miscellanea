cccc193a890004cac4c9f0930c84044e90a73091
Samuel Bear Powell <samuel.powell@uq.edu.au>
2019-01-23 13:49:32 +1000

    add emailer

--- script_utils.py
+++ script_utils.py
@@ -26,3 +26,28 @@ def find_files(paths, exts, recurse=True, quiet=False):
                 file_paths.append(p)
     return file_paths
 
+from smtplib import SMTP
+from email.message import EmailMessage
+class emailer:
+    def __init__(self, host, port, user=None, password=None, useTLS=True):
+        self.smtp = SMTP(host,port)
+        self.useTLS = useTLS
+        self.user = user
+        self.password = password
+    
+    def send(self,to,subject,message,frm=None):
+        if frm is None:
+            frm = self.user
+        msg = EmailMessage()
+        msg.set_content(message)
+        msg['To'] = to
+        msg['From'] = frm
+        msg['Subject'] = subject
+        with self.smtp:
+            self.smtp.connect()
+            if self.useTLS:
+                self.smtp.starttls()
+            if self.user is not None and self.password is not None:
+                self.smtp.login(self.user,self.password)
+            return self.smtp.send_message(msg)
+
